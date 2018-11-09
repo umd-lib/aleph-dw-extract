11c0a017baae9d5a67f273a85eebecb634f99855
Tiffany Schoneboom <thschone@umd.edu>
2018-11-09 16:51:35 -0500

    refactored a2.py working

--- a2.py
+++ a2.py
@@ -2,55 +2,75 @@
 import fileinput
 import pdb
 import sys
-#pdb.set_trace()
 
-
-flags = ''
-old_flag = ''
-old_table = ''
-old_key = ''
-global table, key
-table = ''
-key = ''
-
+# write to filename based on cli argument
 lst_filename = sys.argv[1]
 lib = lst_filename.split('_')[0]
 tab = lst_filename.split('_')[1].split('.')[0]
 filename = lib + "_" + tab + "_b"
 f = open(filename, 'w+')
 
-def dump_it(table, key):
-    global flags, old_table, old_key, old_flag
-    if flags:
-        option = '?'
-        first = flags[0]
-        last = flags[-1]
-        if (first == 'D' and last == 'I'):
-            option = 'U'
-        elif (first == 'I' and last == 'D'):
-            option = 'Z'
-        elif (first =='U' and  last == 'D'):
-            option = 'D'
-        else:
-            option = first
-    	# format output
-    	f.write(option.ljust(10) + ' ' + old_table + ' ' + old_key +'\n')
-    flags = ''
-    old_table = table
-    old_key = key
-    old_flag = '' 
 
-for line in fileinput.input():
-    line = line.rstrip()
-    flag = line[0]
-    timestamp = line[3:23]
-    table = line[25:35]
-    key = line[37:]
+# class to parse lines
+class ParsedLine:
+    def __init__(self, line_string):
+        self.line = line_string.rstrip()
+        self.flag = self.line[0]
+        self.timestamp = self.line[3:23]
+        self.table = self.line[25:35]
+        self.key = self.line[37:]
+        
+# class to save series of actions on an item
+class ActionSeries:
+    def __init__(self, parsed_line):
+        self.key = parsed_line.key
+	self.table = parsed_line.table
+	self.lines = [parsed_line]
+	self.flags = parsed_line.flag
     
-    if (key!=old_key or table != old_table):
-        dump_it(table, key)
-    if (flag != old_flag):
-        flags = flags + flag
-        old_flag = flag
-dump_it(table, key)
+    def add_line(self, parsed_line):
+	self.lines.append(parsed_line)
+	self.flags += parsed_line.flag
+    
+    def write_final_data(self):
+        # determine final flag action
+	first = self.flags[0]
+	last = self.flags[-1]
+	if (first == 'D' and last == 'I'):
+	    option = 'U'
+	elif (first == 'I' and last == 'D'):
+	    option = 'Z'
+	elif (first ==  'U' and last == 'D'):
+	    option = 'D'
+	else:
+	    option = first
+	
+        # format and write output
+	f.write(option.ljust(10) + ' ' + self.table + ' ' + self.key + '\n')
+        
+    
+# create ParsedLine object for every line in input file
+#lines = [ParsedLine(line) for line in fileinput.input()]
+lines = []
+for line in fileinput.input():
+    lines.append(ParsedLine(line))
+# initialize an empty list for action series
+all_action_series = []
+
+# create action series if lines refer to same item
+for line in lines:
+    # check for first iteration
+    if all_action_series:
+	# add lines with matching key to action series
+        last_action_series = all_action_series[-1]
+        if (last_action_series.key == line.key and last_action_series.table == line.table):
+            last_action_series.add_line(line)
+            continue
+    # create new action series for non-matching or first iteration line
+    all_action_series.append(ActionSeries(line))
+
+# write to file
+for a in all_action_series:
+    a.write_final_data()
+# close file after writing
 f.close()
